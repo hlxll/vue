d7c0764381f27841a23205ef85ab01444674f082
hlxll <447404195@qq.com>
2022-03-08 16:00:41 +0800

    插件开发

---
+++ src/plugin/translate.ts
@@ -0,0 +1,18 @@
+
+
+// export default {
+//     install: (app: any, params: any) => {
+//         app.config.globalProperties.$translate = (key: string) => {
+//             return '翻译文字'
+//         }
+
+//     }
+// }
+export default (app: any, params: any) => {
+    console.log(app);
+    console.log(params);
+    app.config.globalProperties.$translate = () => {
+        return '翻译文字'
+    }
+
+}
