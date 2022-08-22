ce23f6aacd1ca0e069b1eeec6c14f426d5549284
hlxll <447404195@qq.com>
2022-08-22 19:13:51 +0800

    merge

--- src/components/apiSetup/setupIndex.vue
+++ src/components/apiSetup/setupIndex.vue
@@ -2,8 +2,9 @@
   <div>插槽显示</div>
 </template>
 
+
 <script>
-import { defineComponent, h, toRefs, reactive } from "vue";
+import { defineComponent, h, toRefs, ref, reactive } from "vue";
 export default defineComponent({
   props: {
     inpData: String
@@ -12,7 +13,20 @@ export default defineComponent({
   setup(props, context) {
     //props需要在上方定义，不定义只会在attrs中引入
     let { inpData } = toRefs(props);
-    let list = reactive([1, 2, 3, 4]);
+
+    /**
+     * reactive其实是对js对象的代理，代理和原对象不想等，如果改变原对象，代理不会改变，只有代理是响应式的
+     * 多个相同对象代理，是想等的，代理的代理和原代理也是想等的
+     */
+    let arr = [1, 2, 3, 4];
+    let list = reactive(arr);
+    /**
+     * ref也是代理，但是可支持任何数据类型,如果直接访问该响应式数据，可以直接使用，不用.value，
+     * 如果不是顶级数据，是object.proxy访问该代理proxy，就不能直接访问，需要使用.value.
+     * 但是如果object也是代理的响应式数据，就可以使用object.proxy了
+     */
+    let count = ref(0);
+    console.log(count);
     let { slots, attrs, expose } = context;
     //expose向父组件暴露数据，暴露实例，正常使用ref，parent，$root等获取的实例是等同于模板的实例，expose可以创建一个独立的实例
     expose({});
@@ -34,3 +48,5 @@ export default defineComponent({
   }
 });
 </script>
+
+
--- src/components/wb/wb_head.vue
+++ src/components/wb/wb_head.vue
@@ -1,53 +1,70 @@
 <template>
-    <div class="head">
-        <button class="loginBtn" >登录</button>
-        <loginModal>
-            <div class="loginModal">
-                <p>请选择如下方式登录微博</p>
-                <p>打开 微博手机APP - 我的 - 扫一扫</p>
-            </div>
-        </loginModal>
-    </div>
+  <div class="head">
+    <button class="loginBtn">登录</button>
+    <loginModal>
+      <div class="loginModal">
+        <p>请选择如下方式登录微博</p>
+        <p>打开 微博手机APP - 我的 - 扫一扫</p>
+      </div>
+      <img :src="qrcodeUrl" />
+    </loginModal>
+  </div>
 </template>
 <script>
-import loginModal from './wb_modal.vue'
+import loginModal from "./wb_modal.vue";
+import { onMounted, getCurrentInstance, reactive, ref } from "vue";
 export default {
-    name: 'wbHead',
-    components: {
-        loginModal
-    },
-    created() { 
-        this.f_createQr()
-    },
-    methods: {
-        f_createQr: function () {
-            this.$axios
-            .request({
-                url: "/admin/qrCode",
-            })
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        }
-    }
-}
+  name: "wbHead",
+  components: {
+    loginModal
+  },
+  setup() {
+    const { proxy } = getCurrentInstance();
+    let state = reactive({
+      qrcodeUrl: "",
+      hasSession: true
+    });
+    let qrcodeUrl = ref("");
+    const methods = {
+      async initQrUrl() {
+        await proxy.$axios
+          .request({
+            url: "/admin/qrCode"
+          })
+          .then(res => {
+            state.qrcodeUrl = res.data;
+            qrcodeUrl.value = res.data;
+            // setInterval(() => {
+            //   proxy.$axios.request({
+            //     url: ""
+            //   });
+            // }, 1000);
+          });
+      }
+    };
+    onMounted(async () => {
+      await methods.initQrUrl();
+    });
+    return {
+      state,
+      qrcodeUrl
+    };
+  }
+};
 </script>
 <style scoped>
-.loginBtn{
-    width: 50px;
-    height: 30px;
-    line-height: 30px;
-    background-color: #ff8200;     
-    color: white;   
-    border: none;
-    padding: 0;
-    border-radius: 10px;
+.loginBtn {
+  width: 50px;
+  height: 30px;
+  line-height: 30px;
+  background-color: #ff8200;
+  color: white;
+  border: none;
+  padding: 0;
+  border-radius: 10px;
 }
-.loginModal{
-    width: 200px;
-    height: 200px;
+.loginModal {
+  width: 200px;
+  height: 200px;
 }
 </style>
